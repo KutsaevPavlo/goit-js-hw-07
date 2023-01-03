da1cb5313fa9b025526b53664e65f3bb260e61a9
Pavlo <p1kutsaev@gmail.com>
2023-01-03 16:10:25 +0200

    update 01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log(createGellaryItems(galleryItems));
+
+function createGellaryItems(galleryItems){
+    const imageCards = galleryItems.map(galleryItem => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                class="gallery__image"
+                src="small-image.jpg"
+                data-source="large-image.jpg"
+                alt="Image description"
+                />
+            </a>
+        </div>
+    `;
+    });
+    console.log(imageCards);
+}
+
+
